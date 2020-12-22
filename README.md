88aa501176f85cea22203ab467262ae947a642c3
ZYROUGE <naveencnaveen96@gmail.com>
2020-12-22 15:33:11 +0530

    Ep 1

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Discord.js Tutorial
+# ExpressJS Tutorial
 
+- **The Basics**
 - Episode No. 1
-- Youtube - https://youtu.be/B1wiAiBE0PU
-- Article - https://guides.zyrouge.gq/discordjs/coding/
+- Youtube - https://youtu.be/qv2MjXvSLsw
+- Article -
 - Discord - https://zyrouge.gq/discord
 
-[![Discord.js Bot - Getting Started (Ep. 1)](https://img.youtube.com/vi/B1wiAiBE0PU/0.jpg)](https://www.youtube.com/watch?v=B1wiAiBE0PU "Discord.js Bot - Getting Started (Ep. 1)")
+[![ExpressJS - The Basics | Episode 1 | Zyrouge Development](https://img.youtube.com/vi/qv2MjXvSLsw/0.jpg)](https://www.youtube.com/watch?v=qv2MjXvSLsw "ExpressJS - The Basics | Episode 1 | Zyrouge Development")
--- index.js
+++ index.js
@@ -1,5 +1,70 @@
-const Discord = require("discord.js");
-const fs = require('fs');
-const client = new Discord.Client()
+const express = require("express");
 
-client.login(process.env.TOKEN)
+const PORT = 8080;
+
+const users = [
+    {
+        name: "user1234",
+        age: 14,
+        level: 10
+    },
+    {
+        name: "user156",
+        age: 20,
+        level: 15
+    },
+    {
+        name: "user1",
+        age: 30,
+        level: 5
+    }
+];
+
+const init = async () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    /* Simple Ping */
+    app.get("/ping", (req, res) => {
+        res.send("Pong!");
+    });
+
+    /* Simple JSON */
+    app.get("/json-simple", (req, res) => {
+        res.json({
+            ping: "pong"
+        });
+    });
+
+    /* Simple Post Request */
+    app.post("/post-simple", (req, res) => {
+        console.log(req.body);
+        res.json(req.body);
+    });
+
+    /* Params */
+    app.get("/users/:user", (req, res) => {
+        const name = req.params.user;
+        const user = users.find(x => x.name === name);
+        if (!user) return res.send("No User");
+        res.send(user);
+    });
+
+    /* Query */
+    app.get("/users/:user/info", (req, res) => {
+        const name = req.params.user;
+        const user = users.find(x => x.name === name);
+        if (!user) return res.send("No User");
+        const key = req.query.key;
+        if (!key) return res.send("Specify a key!");
+        res.send(`${user[key]}`);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`);
+    });
+}
+
+init();
